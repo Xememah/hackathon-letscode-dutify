d95d892b073c48f6219ce38b0981ca4caca028e4
Marek Kochanowski <sii-hackathon@kochanow.ski>
2017-11-18 16:24:11 +0100

    Remove useless buttons from header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -25,14 +25,14 @@
     </v-navigation-drawer>
     <v-toolbar fixed app :clipped-left="clipped">
       <v-toolbar-side-icon @click.stop="drawer = !drawer" light></v-toolbar-side-icon>
-      <v-btn
+      <!--<v-btn
         icon
         light
         @click.stop="miniVariant = !miniVariant"
       >
         <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
       </v-btn>
-      <v-btn
+       <v-btn
         icon
         light
         @click.stop="clipped = !clipped"
@@ -45,22 +45,21 @@
         @click.stop="fixed = !fixed"
       >
         <v-icon>remove</v-icon>
-      </v-btn>
+      </v-btn> -->
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
+      <!-- <v-btn
         icon
         light
         @click.stop="rightDrawer = !rightDrawer"
       >
         <v-icon>menu</v-icon>
-      </v-btn>
+      </v-btn> -->
     </v-toolbar>
     <v-content>
       <v-container fluid>
         <v-slide-y-transition mode="out-in">
           <v-layout column align-center>
-            <img src="/static/img/v.png" alt="Vuetify.js" class="mb-5">
             <router-view></router-view>
           </v-layout>
         </v-slide-y-transition>
