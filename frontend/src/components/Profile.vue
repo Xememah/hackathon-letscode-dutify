4a9012f4a1eaaddf706aae66752ac85e0449e7bb
symbor <yghritte@gmail.com>
2017-11-18 18:39:01 +0100

    Add Profile page

--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    Profile
-  </div>
+  <v-container class="text-xs-center">
+    <v-avatar class="indigo" size="130px">
+      <img src="http://i.pravatar.cc/300">
+    </v-avatar>
+
+    <p class="text-xs-center">
+      <h2>Weronika Symbor</h2>
+    </p>
+  </v-container>
 </template>
