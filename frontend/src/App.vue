355d6977a437a0f3626a19390d4a9d12a698e9ce
Marek Kochanowski <sii-hackathon@kochanow.ski>
2017-11-18 17:01:49 +0100

    Update default colors

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,14 @@
 <template>
   <v-app light>
-    <v-navigation-drawer
+    <!-- <v-navigation-drawer
       fixed
       :mini-variant="miniVariant"
       :clipped="clipped"
       v-model="drawer"
       app
     >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-          exact
-        >
-          <v-list-tile-action>
-            <v-icon light v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar fixed app :clipped-left="clipped">
+    </v-navigation-drawer> -->
+    <v-toolbar fixed app :clipped-left="clipped" color="white" flat>
       <v-toolbar-side-icon @click.stop="drawer = !drawer" light></v-toolbar-side-icon>
       <!--<v-btn
         icon
@@ -79,6 +64,7 @@
           <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
         </v-list-tile>
       </v-list>
+
     </v-navigation-drawer>
     <v-footer :fixed="fixed" app>
       <span>&copy; 2017</span>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,8 +7,17 @@ import 'vuetify/dist/vuetify.css'
 import App from './App'
 import router from './router'
 
-Vue.use(Vuetify)
+// Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#000',
+    secondary: '#b0bec5',
+    accent: '#8c9eff',
+    error: '#b71c1c'
+  }
+})
 Vue.config.productionTip = false
+
 
 /* eslint-disable no-new */
 new Vue({
