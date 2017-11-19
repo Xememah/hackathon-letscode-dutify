bdbda0299fe86912779b3d93d395d0d73d7ef28f
Marek Kochanowski <sii-hackathon@kochanow.ski>
2017-11-19 02:07:13 +0100

    [Frontend] Insignificant changes, just like with fixing my life

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <v-app light flat>
     <v-navigation-drawer fixed v-model="drawer" app>
+      <v-container>
+      <img src="/static/img/logo/dark-logo.svg" alt="Dutify" style="height: 25px;">
+      </v-container>
     </v-navigation-drawer>
     <v-toolbar app color="white" flat>
       <v-toolbar-side-icon @click.stop="drawer = !drawer" light></v-toolbar-side-icon>
--- frontend/src/components/Homepage.vue
+++ frontend/src/components/Homepage.vue
@@ -1,25 +1,19 @@
 <template>
   <v-flex>
     <v-card>
-    <v-list subheader dense>
-      <v-subheader>Your progress</v-subheader>
-      <v-list-tile avatar v-for="item in items" v-bind:key="item.title" :disabled="item.disabled">
-        <v-list-tile-avatar>
-          <img v-bind:src="item.avatar"/>
-        </v-list-tile-avatar>
-        <v-list-tile-content>
-          <v-list-tile-title v-html="item.title"></v-list-tile-title>
-        </v-list-tile-content>
-        <v-list-tile-action><strong>{{ item.points }} points</strong></v-list-tile-action>
-      </v-list-tile>
-    </v-list>
-
+      <v-list subheader dense>
+        <v-subheader>Your progress</v-subheader>
+        <v-list-tile avatar v-for="item in items" v-bind:key="item.title" :disabled="item.disabled">
+          <v-list-tile-avatar>
+            <img v-bind:src="item.avatar"/>
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title v-html="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+          <v-list-tile-action><strong>{{ item.points }} points</strong></v-list-tile-action>
+        </v-list-tile>
+      </v-list>
     </v-card>
-    <v-container>
-      <v-card>
-        test
-      </v-card>
-    </v-container>
   </v-flex>
 </template>
 
