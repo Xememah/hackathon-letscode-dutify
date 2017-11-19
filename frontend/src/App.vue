0099892ad943cfd6c87bd5d06973f7d9b17741af
Marek Kochanowski <sii-hackathon@kochanow.ski>
2017-11-19 02:46:54 +0100

    [Frontend] Implement account authorization

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,16 +5,19 @@
       <img src="/static/img/logo/dark-logo.svg" alt="Dutify" style="height: 25px;">
       </v-container>
     </v-navigation-drawer>
+    
     <v-toolbar app color="white" flat>
       <v-toolbar-side-icon @click.stop="drawer = !drawer" light></v-toolbar-side-icon>
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
     </v-toolbar>
+    
     <v-layout>
       <v-slide-y-transition mode="out-in">
         <router-view></router-view>
       </v-slide-y-transition>
     </v-layout>
+
     <v-bottom-nav absolute shift :value="true" :active.sync="accent_color" :color="computedColor">
       <v-btn href="#/" dark>
         <span>Home</span>
--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,6 +1,38 @@
 <template>
-  <v-card class="black">
-  <img src="/static/img/logo/white-logo.svg" alt="Dutify"/>
-  test tekst
-  </v-card>
+  <v-flex>
+    <v-card>
+      <v-card-text>
+        <v-form v-model="valid">
+          <v-text-field
+            color="white"
+            label="E-mail"
+            v-model="email"
+            :rules="emailRules"
+            required
+          ></v-text-field>
+          <v-text-field
+            label="Password"
+            v-model="password"
+            required
+          ></v-text-field>
+          <v-btn color="primary">Log in</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-flex>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        valid: false,
+        email: '',
+        emailRules: [
+          (v) => !!v || 'E-mail is required',
+          (v) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+        ]
+      }
+    }
+  }
+</script>
